4b6a426a6c205b5bbe5c4380efa39fca7692f8fe
glomidan <braux.danael@gmail.com>
2021-11-19 17:48:04 +0100

    tests get stdin

--- src/main.c
+++ src/main.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
-
-int lexer();
+#include <unistd.h>
 
 int main(int argc, char* argv[])
 {
-    printf("hello main");
-    return 1;
+    if (argc == 1)
+    {
+        printf("sh$ ");
+        int fd = 0; // 0 = stdin
+        char buf[32];
+        read(fd, buf, 31);
+        printf("%s", buf);
+        printf("%s", argv);
+    }
+    else
+    {
+        printf("exec some command");
+    }
+    return 0;
 }
