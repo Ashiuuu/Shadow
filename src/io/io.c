7a0698c38ff8a76dfe35aff5f8435b63db01c2e6
Ashiuuu <clement.milisavljevic@epita.fr>
2021-12-14 15:01:46 +0100

    Added opening stream from string or file

--- src/io/io.c
+++ src/io/io.c
@@ -1,3 +1,25 @@
 #include "io.h"
 
+FILE *string_to_stream(char *buffer)
+{
+    FILE *stream = fmemopen(buffer, strlen(buffer), "r");
+    if (stream == NULL)
+    {
+        fprintf(stderr, "Unable to open stream for buffer %s\n", buffer);
+        abort();
+    }
 
+    return stream;
+}
+
+FILE *open_file(char *filename)
+{
+    FILE *file = fopen(filename, "r");
+    if (file == NULL)
+    {
+        fprintf(stderr, "Unable to open file %s\n", filename);
+        abort();   
+    }
+
+    return file;
+}
--- src/io/io.h
+++ src/io/io.h
@@ -1,3 +1,10 @@
 #pragma once
 
+#define _GNU_SOURCE
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+FILE *string_to_stream(char *buffer);
+FILE *open_file(char *filename);
