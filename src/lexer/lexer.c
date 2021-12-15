baa00ac15dcf45e8c18ef5b714ecdf48ab09a26c
Ashiuuu <clement.milisavljevic@epita.fr>
2021-12-15 12:13:23 +0100

    Added basic lexer structure

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -1,6 +1,25 @@
 #include "lexer.h"
 
-void lexer()
+struct lexer *lexer_new(FILE *input_stream)
 {
-    return;
+    struct lexer *ret = malloc(sizeof(struct lexer));
+    if (ret == NULL)
+    {
+        fprintf(stderr, "Unable to create lexer");
+        abort();
+    }
+
+    ret->input = input_stream;
+    ret->current_token = NULL;
+
+    return ret;
+}
+
+void lexer_free(struct lexer *lexer)
+{
+    if (lexer->current_token != NULL)
+        free(lexer->current_token);
+    if (lexer->input != NULL)
+        fclose(lexer->input);
+    free(lexer);
 }
--- src/lexer/lexer.h
+++ src/lexer/lexer.h
@@ -1,3 +1,17 @@
 #pragma once
 
-void lexer();
+#include <stdio.h>
+
+#include "tokens.h"
+
+struct lexer
+{
+    FILE *input;
+    struct token *current_token;
+};
+
+struct lexer *lexer_new(FILE *input_stream);
+void lexer_free(struct lexer *lexer);
+
+struct token *lexer_peek(struct lexer *lexer);
+struct token *lexer_pop(struct lexer *lexer);
