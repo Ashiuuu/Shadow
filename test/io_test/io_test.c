8d008bec24f0a8e5b7b3eb5e1a3aad52d80e2272
erwan benard <erwan.benard@epita.fr>
2021-12-15 12:24:07 +0100

    Test Criterion

---
+++ test/io_test/io_test.c
@@ -0,0 +1,14 @@
+#include <criterion/criterion.h>
+#include "io.h"
+
+/*Test(Io_Test, No_String)
+{
+    string_to_stream("");
+}*/
+
+Test(Io_test, "Simple String")
+{
+    FILE *stream = string_to_stream("Hello World");
+    cr_assert_str_not_empty(fgetc(stream));
+    cr_assert_str_eq(fgetc(stream), 'e');
+}
