d9119549096d9c9a8e7b00e5dfd80ccd06050fe1
Ashiuuu <ushiuako@gmail.com>
2021-12-17 14:36:33 +0100

    Updated io structure

--- src/io/io.h
+++ src/io/io.h
@@ -4,5 +4,19 @@
 #include <string.h>
 #include <stdlib.h>
 
+struct INPUT
+{
+    char current_char;
+    char next_char;
+    FILE *stream;
+};
+
+struct INPUT *new_input(FILE *stream);
+struct INPUT *input_from_string(char *s);
+struct INPUT *input_from_stdin();
+struct INPUT *input_from_file(char *filename);
+char pop_char(struct INPUT *input);
+
+
 FILE *string_to_stream(char *buffer);
 FILE *open_file(char *filename);
