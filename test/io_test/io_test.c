90adf125bcbaaf83910e0148976674da2c941e83
Danael BRAUX <danael.braux@epita.fr>
2021-12-17 16:57:13 +0100

    update io_test.c

--- test/io_test/io_test.c
+++ test/io_test/io_test.c
@@ -2,18 +2,29 @@
 #include "io.h"
 
 /*Test(Io_Test, No_String)
-{
-    string_to_stream("");
-}*/
+  {
+  string_to_stream("");
+  }*/
 
 /*Test(Io_test, "Simple String")
-{
-    FILE *stream = string_to_stream("Hello World");
-    cr_assert_str_not_empty(fgetc(stream));
-    cr_assert_str_eq(fgetc(stream), 'e');
-}*/
+  {
+  FILE *stream = string_to_stream("Hello World");
+  cr_assert_str_not_empty(fgetc(stream));
+  cr_assert_str_eq(fgetc(stream), 'e');
+  }*/
 
 Test(Sample, test_is_null)
 {
     cr_assert_null(1);
+}
+
+int main(int argc, char *argv[]) {
+    struct criterion_test_set *tests = criterion_initialize();
+
+    int result = 0;
+    if (criterion_handle_args(argc, argv, true))
+        result = !criterion_run_all_tests(tests);
+
+    criterion_finalize(tests);
+    return result;
 }
