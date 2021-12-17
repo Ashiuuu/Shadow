07ca750b5173796fc5e4ee69dd89484176155542
Ashiuuu <ushiuako@gmail.com>
2021-12-17 16:11:24 +0100

    Added basic words lexer

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -1,5 +1,20 @@
 #include "lexer.h"
 
+int is_alpha(char c)
+{
+    return ((c <= 'z' && c >= 'a') || (c <= 'Z' && c >= 'A'));
+}
+
+int is_num(char c)
+{
+    return (c <= '9' && c >= '0');
+}
+
+int is_alphanum(char c)
+{
+    return (is_alpha(c) || is_num(c));
+}
+
 struct lexer *lexer_new(FILE *input_stream)
 {
     struct lexer *ret = malloc(sizeof(struct lexer));
@@ -11,6 +26,7 @@ struct lexer *lexer_new(FILE *input_stream)
 
     ret->input = input_stream;
     ret->current_token = NULL;
+    ret->word_lexer = new_word_lexer();
 
     return ret;
 }
@@ -22,4 +38,40 @@ void lexer_free(struct lexer *lexer)
     if (lexer->input != NULL)
         fclose(lexer->input);
     free(lexer);
+}
+
+struct token *lexer_peek(struct lexer *lexer)
+{
+    if (lexer->current_token == NULL)
+        read_until_new_token(lexer);
+    return lexer->current_token;
+}
+
+struct token *lexer_pop(struct lexer *lexer)
+{
+    lexer = lexer;
+}
+
+struct token *read_until_new_token(struct lexer *lexer)
+{
+    if (lexer->input->current_char == EOF)
+    {
+        // new EOF token
+    }
+
+    while (lexer->word_lexer->state == LEXER_CONT)
+    {
+        enum lexer_state state = word_lexer_consume_char(lexer->word_lexer, lexer->input->current_char);
+        if (state == LEXER_ERROR)
+        {
+            fprintf(stderr, "unexpected char %c\n", lexer->input->current_char);
+            lexer->current_token = token_new(TOKEN_ERROR);
+            return lexer->current_token;
+        }
+    }
+    if (lexer->current_token != NULL)
+        token_free(lexer->current_token);
+    lexer->current_token = token_new(TOKEN_WORDS);
+    lexer->current_token->value = lexer->word_lexer->value;
+    return lexer->current_token;
 }
--- src/lexer/lexer.h
+++ src/lexer/lexer.h
@@ -1,17 +1,68 @@
 #pragma once
 
 #include <stdio.h>
+#include <string.h>
 
 #include "tokens.h"
 
+// Utilities
+int is_num(char c);
+int is_alpha(char c);
+int is_alphanum(char c);
+
+
+enum lexer_state
+{
+    LEXER_CONT,
+    LEXER_ERROR,
+    LEXER_ACCEPT
+};
+
 struct lexer
 {
-    FILE *input;
+    INPUT *input;
     struct token *current_token;
+    struct word_len *word_lexer;
+    //struct keyword_lexer **keyword_list;
 };
+
+
+// Lexer that recognize words, such as variable name,
+
+struct word_lexer
+{
+    char *value;
+    size_t len;
+    size_t capacity;
+    enum lexer_state state;
+};
+
+struct word_lexer *new_word_lexer();
+void reset_word_lexer(struct word_lexer *lexer);
+enum lexer_state word_lexer_consume_char(struct word_lexer *lexer, char c);
+
+// Lexer that recognize keywords : if, else, fi, etc...
+
+struct keyword_lexer
+{
+    char *keyword;
+    size_t word_len;
+    size_t pos;
+    enum lexer_state state;
+};
+
+struct keyword_lexer *new_keyword_lexer(const char *word);
+void reset_keyword_lexer(struct keyword_lexer *lexer);
+enum lexer_state keyword_lexer_consume_char(struct keyword_lexer *lexer, char c);
+
+
+
+
 
 struct lexer *lexer_new(FILE *input_stream);
 void lexer_free(struct lexer *lexer);
 
+
+struct token *read_until_new_token(struct lexer *lexer);
 struct token *lexer_peek(struct lexer *lexer);
 struct token *lexer_pop(struct lexer *lexer);
