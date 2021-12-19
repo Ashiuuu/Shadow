e4465bb09e66fdfdaa8105298f6f81711b790348
Ashiuuu <ushiuako@gmail.com>
2021-12-19 01:23:22 +0100

    Fixed missing EOF test

--- src/lexer/word_lexer.c
+++ src/lexer/word_lexer.c
@@ -16,6 +16,8 @@ void reset_word_lexer(struct word_lexer *lexer)
 {
     if (lexer->value != NULL)
         free(lexer->value);
+    lexer->capacity = 10;
+    lexer->value = xmalloc(sizeof(char) * lexer->capacity);
     lexer->len = 0;
     lexer->state = LEXER_CONT;
 }
@@ -24,16 +26,6 @@ enum lexer_state word_lexer_consume_char(struct word_lexer *lexer, struct INPUT 
 {
     if (lexer->state == LEXER_ERROR)
         return LEXER_ERROR;
-
-
-    if (input->next_char == ' ' || input->next_char == ';' || input->next_char == '\n') // end of word
-    {
-        lexer->state = LEXER_ACCEPT;
-        lexer->capacity = lexer->len + 1;
-        lexer->value = xrealloc(lexer->value, sizeof(char) * lexer->capacity);
-        lexer->value[lexer->len] = '\0';
-        return LEXER_ACCEPT;
-    }
 
     if (is_alphanum(input->current_char))
     {
@@ -44,7 +36,17 @@ enum lexer_state word_lexer_consume_char(struct word_lexer *lexer, struct INPUT 
             lexer->value = xrealloc(lexer->value, sizeof(char) * lexer->capacity);
         }
         lexer->value[lexer->len] = input->current_char;
+        lexer->len++;
         return LEXER_CONT;
+    }
+
+    if (input->current_char == ' ' || input->current_char == ';' || input->current_char == '\n' || input->current_char == EOF) // end of word
+    {
+        lexer->state = LEXER_ACCEPT;
+        lexer->capacity = lexer->len + 1;
+        lexer->value = xrealloc(lexer->value, sizeof(char) * lexer->capacity);
+        lexer->value[lexer->len] = '\0';
+        return LEXER_ACCEPT;
     }
 
     // not a valid char
