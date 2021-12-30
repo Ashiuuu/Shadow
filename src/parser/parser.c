c38c25c4908fa4f6f100823877d33d06f50dfdbc
Ashiuuu <clement.milisavljevic@epita.fr>
2021-12-30 16:50:14 +0100

    Added if parsing

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -60,15 +60,6 @@ enum parser_status parser_simple_command(struct ast_node **ast, struct lexer *in
 
 // Grammar :
 // list: command (';' command)* [';']
-// command
-// ;
-//   - finito
-//   - command
-//     - finito
-//     - ;
-//       - finito
-//       - command
-//         - ...
 enum parser_status parse_command_list(struct ast_node **ast, struct lexer *input)
 {
     *ast = new_list_node();
@@ -111,5 +102,150 @@ enum parser_status parse_command_list(struct ast_node **ast, struct lexer *input
             return PARSER_ERROR;
         }
     }
+    return PARSER_OK;
+}
+
+
+// Grammar:
+// ('\n')* and_or ((';'|'\n') ('\n')* and_or)* [(';'|'\n') ('\n')*]
+// and_or = command for now
+enum parser_status parse_compound_list(struct ast_node **ast, struct lexer *input)
+{
+    *ast = new_list_node();
+
+    struct token *tok = lexer_peek(input);
+    while (tok->type == TOKEN_EOL) // ('\n')*
+        lexer_pop(input);
+
+    // first and_or
+    struct ast_node *com = NULL;
+    parser_simple_command(&com, input);
+    list_node_push(*ast, com);
+
+    tok = lexer_peek(input);
+    while (tok->type == TOKEN_SEMICOL || tok->type == TOKEN_EOL) // ((';'|'\n') ('\n')* and_or)*
+    {
+        tok = lexer_peek(input);
+
+        if (tok->type != TOKEN_EOL && tok->type != TOKEN_SEMICOL)
+            break;
+
+        // we saw ; or \n, pop token
+        tok = lexer_pop(input);
+        while (tok->type == TOKEN_EOL)
+            tok = lexer_pop(input);
+
+        // we either have a and_or or we are over
+        if (tok->type == TOKEN_WORDS) // we have a command
+        {
+            struct ast_node *new_com = NULL;
+            parser_simple_command(&new_com, input);
+            list_node_push(*ast, new_com);
+        }
+        else // compound list is over
+        {
+            return PARSER_OK;
+        }
+        tok = lexer_peek(input);
+    }
+
+    // we shouldn't get here
+    return PARSER_ERROR;
+}
+
+
+//Grammar:
+//   ELSE compound_list
+// | ELIF compound_list THEN compound_list [else_clause]
+enum parser_status parse_else_clause(struct ast_node **ast, struct lexer *input)
+{
+    struct token *tok = lexer_peek(input);
+    *ast = new_if_node();
+
+    if (tok->type == TOKEN_ELSE)
+    {
+        struct ast_node *body = NULL;
+        parse_compound_list(&body, input);
+        (*ast)->data.ast_if.body_list = body;
+        return PARSER_OK;
+    }
+    else if (tok->type == TOKEN_ELIF)
+    {
+        struct ast_node *cond = NULL;
+        struct ast_node *body = NULL;
+        struct ast_node *elsif = NULL;
+
+        parse_compound_list(&cond, input);
+        (*ast)->data.ast_if.condition = cond;
+
+        tok = lexer_peek(input);
+        if (tok->type != TOKEN_THEN)
+        {
+            fprintf(stderr, "missing THEN token\n");
+            return PARSER_ERROR;
+        }
+        lexer_pop(input);
+        parse_compound_list(&body, input);
+        (*ast)->data.ast_if.body_list = body;
+
+        tok = lexer_peek(input);
+        if (tok->type == TOKEN_ELIF || tok->type == TOKEN_ELSE)
+        {
+            parse_else_clause(&elsif, input);
+            (*ast)->data.ast_if.elif = elsif;
+            return PARSER_OK;
+        }
+
+        return PARSER_OK;
+    }
+
+    return PARSER_ERROR;
+}
+
+// Grammar:
+// IF compound_list THEN compound_list [else_clause] FI
+enum parser_status parse_rule_if(struct ast_node **ast, struct lexer *input)
+{
+    struct token *tok = lexer_peek(input);
+    if (tok->type != TOKEN_IF)
+    {
+        fprintf(stderr, "expected IF\n");
+        return PARSER_ERROR;
+    }
+
+    *ast = new_if_node();
+
+    lexer_pop(input);
+    struct ast_node *compound = NULL;
+    parse_compound_list(&compound, input);
+    (*ast)->data.ast_if.condition = compound;
+
+    tok = lexer_peek(input);
+    if (tok->type != TOKEN_THEN)
+    {
+        fprintf(stderr, "expected THEN\n");
+        return PARSER_ERROR;
+    }
+
+    lexer_pop(input);
+    compound = NULL;
+    parse_compound_list(&compound, input);
+    (*ast)->data.ast_if.body_list = compound;
+
+    tok = lexer_peek(input);
+    if (tok->type == TOKEN_ELIF || tok->type == TOKEN_ELSE)
+    {
+        struct ast_node *elsif = NULL;
+        parse_else_clause(&elsif, input);
+        (*ast)->data.ast_if.elif = elsif;
+        tok = lexer_peek(input);
+    }
+
+    if (tok->type == TOKEN_FI)
+    {
+        fprintf(stderr, "expected FI\n");
+        return PARSER_ERROR;
+    }
+
     return PARSER_OK;
 }
