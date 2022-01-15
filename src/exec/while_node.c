d25a0a81520bfe1a41bc8b61b4f036d7e6ed9065
Ashiuuu <clement.milisavljevic@epita.fr>
2022-01-15 20:48:28 +0100

    Added while loop execution

---
+++ src/exec/while_node.c
@@ -0,0 +1,59 @@
+#include "exec.h"
+
+struct ast_node *new_while_node()
+{
+    struct ast_node *ret = xmalloc(sizeof(struct ast_node));
+    ret->type = NODE_WHILE;
+    ret->data.ast_while.condition = NULL;
+    ret->data.ast_while.body_list = NULL;
+
+    return ret;
+}
+
+void free_while_node(struct ast_node *node)
+{
+    if (node->type != NODE_WHILE)
+    {
+        fprintf(stderr, "trying to free non while node\n");
+        return;
+    }
+
+    if (node != NULL)
+    {
+        if (node->data.ast_while.condition != NULL)
+            free_node(node->data.ast_while.condition);
+        if (node->data.ast_while.body_list != NULL)
+            free_node(node->data.ast_while.body_list);
+        free(node);
+    }
+}
+int exec_while_node(struct ast_node *node)
+{
+    if (node->type != NODE_WHILE)
+    {
+        fprintf(stderr, "trying to execute non while node\n");
+        return -1;
+    }
+
+    if (node->data.ast_while.condition == NULL)
+    {
+        fprintf(stderr, "missing condition node [while]\n");
+        return -1;
+    }
+
+    if (node->data.ast_while.body_list == NULL)
+    {
+        fprintf(stderr, "missing body node [while]\n");
+        return -1;
+    }
+
+    int cond = exec_node(node->data.ast_while.condition);
+    int return_status = 0;
+    while (cond == 0)
+    {
+        return_status = exec_node(node->data.ast_while.body_list);
+        cond = exec_node(node->data.ast_while.condition);
+    }
+
+    return return_status;
+}
