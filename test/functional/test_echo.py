905fec5ca395b0d3ea167c48efe8a1082be3df26
Nicolas Sutter <nicolas.sutter@epita.fr>
2022-01-20 19:24:20 +0100

    leak surveillance

--- test/functional/test_echo.py
+++ test/functional/test_echo.py
@@ -6,77 +6,137 @@ import pytest
 def test_echo():
     result = subprocess.run(["./42sh", "-c", "echo Hello"], stdout=subprocess.PIPE).stdout.decode('utf-8')
     model = subprocess.run(["bash", "--posix","-c", "echo Hello"], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    
+    result_err = subprocess.run(["./42sh", "-c", "echo Hello"], stderr=subprocess.PIPE).stderr.decode('utf-8')
+    model_err = subprocess.run(["bash", "--posix","-c", "echo Hello"], stderr=subprocess.PIPE).stderr.decode('utf-8')
+    
     assert result == model
+    assert result_err == model_err
 
 @pytest.mark.timeout(2)
 def test_echo_bis():
     result = subprocess.run(["./42sh", "-c", "echo Hello"], stdout=subprocess.PIPE).stdout.decode('utf-8')
     model = subprocess.run(["bash", "--posix","-c", "echo Hello"], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    
+    result_err = subprocess.run(["./42sh", "-c", "echo Hello"], stderr=subprocess.PIPE).stderr.decode('utf-8')
+    model_err = subprocess.run(["bash", "--posix","-c", "echo Hello"], stderr=subprocess.PIPE).stderr.decode('utf-8')
+    
     assert result == model
+    assert result_err == model_err
 
 @pytest.mark.timeout(2)
 def test_echo_middle():
-    result = os.popen("./42sh -c \"echo Hello Friend\"").read()
+    result = subprocess.run(["./42sh","-c", "echo Hello Friend"], stdout=subprocess.PIPE).stdout.decode('utf-8')
     model = subprocess.run(["bash", "--posix","-c", "echo Hello Friend"], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    
+    result_err = subprocess.run(["./42sh", "-c", "echo Hello Friend"], stderr=subprocess.PIPE).stderr.decode('utf-8')
+    model_err = subprocess.run(["bash", "--posix","-c", "echo Hello Friend"], stderr=subprocess.PIPE).stderr.decode('utf-8')
+    
     assert result == model
+    assert result_err == model_err
 
 @pytest.mark.timeout(2)
 def test_echo_middle_single_quote():
-    result = os.popen("./42sh -c \"echo 'Hello Friend'\"").read()
+    result = subprocess.run(["./42sh","-c", "echo 'Hello Friend'"], stdout=subprocess.PIPE).stdout.decode('utf-8')
     model = subprocess.run(["bash", "--posix","-c", "echo 'Hello Friend'"], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    
+    result_err = subprocess.run(["./42sh","-c", "echo 'Hello Friend'"], stderr=subprocess.PIPE).stderr.decode('utf-8')
+    model_err = subprocess.run(["bash", "--posix","-c", "echo 'Hello Friend'"], stderr=subprocess.PIPE).stderr.decode('utf-8')
+    
     assert result == model
+    assert result_err == model_err
 
 @pytest.mark.timeout(2)
 def test_echo_middle_double_quote():
     result = subprocess.run(["./42sh", "-c", "echo \"Hello Friend\""], stdout=subprocess.PIPE).stdout.decode('utf-8')
     model = subprocess.run(["bash", "--posix","-c", "echo \"Hello Friend\""], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    
+    result_err = subprocess.run(["./42sh", "-c", "echo \"Hello Friend\""], stderr=subprocess.PIPE).stderr.decode('utf-8')
+    model_err = subprocess.run(["bash", "--posix","-c", "echo \"Hello Friend\""], stderr=subprocess.PIPE).stderr.decode('utf-8')
+    
     assert result == model
+    assert result_err == model_err
 
 @pytest.mark.timeout(2)
 def test_echo_long():
-    result = os.popen("./42sh -c \"echo Hello My Friend It is a beautifull day to die\"").read()
+    result = subprocess.run(["./42sh","-c", "echo Hello My Friend It is a beautifull day to die"], stdout=subprocess.PIPE).stdout.decode('utf-8')
     model = subprocess.run(["bash", "--posix","-c", "echo Hello My Friend It is a beautifull day to die"], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    
+    result_err = subprocess.run(["./42sh","-c", "echo Hello My Friend It is a beautifull day to die"], stderr=subprocess.PIPE).stderr.decode('utf-8')
+    model_err = subprocess.run(["bash", "--posix","-c", "echo Hello My Friend It is a beautifull day to die"], stderr=subprocess.PIPE).stderr.decode('utf-8')
+    
     assert result == model
+    assert result_err == model_err
 
 # les points cassent tout
 @pytest.mark.timeout(2)
 def test_echo_long_point():
-    result = os.popen("./42sh -c \"echo Hello My Friend. It is a beautifull day to die.\"").read()
+    result = subprocess.run(["./42sh","-c", "echo Hello My Friend. It is a beautifull day to die."], stdout=subprocess.PIPE).stdout.decode('utf-8')
     model = subprocess.run(["bash", "--posix","-c", "echo Hello My Friend. It is a beautifull day to die."], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    
+    result_err = subprocess.run(["./42sh","-c", "echo Hello My Friend. It is a beautifull day to die."], stderr=subprocess.PIPE).stderr.decode('utf-8')
+    model_err = subprocess.run(["bash", "--posix","-c", "echo Hello My Friend. It is a beautifull day to die."], stderr=subprocess.PIPE).stderr.decode('utf-8')
+    
     assert result == model
+    assert result_err == model_err
 
 # apostrophe casse tout
 @pytest.mark.timeout(2)
 def test_echo_long_apostrophe():
-    result = os.popen("./42sh -c \"echo Hello My Friend. It\'s a beautifull day to die.\"").read()
+    result = subprocess.run(["./42sh","-c", "echo Hello My Friend. It\'s a beautifull day to die."], stdout=subprocess.PIPE).stdout.decode('utf-8')
     model = subprocess.run(["bash", "--posix","-c", "echo Hello My Friend. It\'s a beautifull day to die."], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    
+    result_err = subprocess.run(["./42sh","-c", "echo Hello My Friend. It\'s a beautifull day to die."], stderr=subprocess.PIPE).stderr.decode('utf-8')
+    model_err = subprocess.run(["bash", "--posix","-c", "echo Hello My Friend. It\'s a beautifull day to die."], stderr=subprocess.PIPE).stderr.decode('utf-8')
+    
     assert result == model
+    assert result_err == model_err
 
 # virgule casse tout
 @pytest.mark.timeout(2)
 def test_echo_long_comma():
-    result = os.popen("./42sh -c \"echo Hello My Friend, It is a beautifull day to die.\"").read()
+    result = subprocess.run(["./42sh","-c", "echo Hello My Friend, It is a beautifull day to die."], stdout=subprocess.PIPE).stdout.decode('utf-8')
     model = subprocess.run(["bash", "--posix","-c", "echo Hello My Friend, It is a beautifull day to die."], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    
+    result_err = subprocess.run(["./42sh","-c", "echo Hello My Friend, It is a beautifull day to die."], stderr=subprocess.PIPE).stderr.decode('utf-8')
+    model_err = subprocess.run(["bash", "--posix","-c", "echo Hello My Friend, It is a beautifull day to die."], stderr=subprocess.PIPE).stderr.decode('utf-8')
+    
     assert result == model
+    assert result_err == model_err
 
 # all qui casse tout
 @pytest.mark.timeout(2)
 def test_echo_long_hard():
-    result = os.popen("./42sh -c \"echo Hello My Friend, It's a beautifull day. To die !\"").read()
+    result = subprocess.run(["./42sh","-c", "echo Hello My Friend, It's a beautifull day. To die !"], stdout=subprocess.PIPE).stdout.decode('utf-8')
     model = subprocess.run(["bash", "--posix","-c", "echo Hello My Friend, It's a beautifull day. To die !"], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    
+    result_err = subprocess.run(["./42sh","-c", "echo Hello My Friend, It's a beautifull day. To die !"], stderr=subprocess.PIPE).stderr.decode('utf-8')
+    model_err = subprocess.run(["bash", "--posix","-c", "echo Hello My Friend, It's a beautifull day. To die !"], stderr=subprocess.PIPE).stderr.decode('utf-8')
+    
     assert result == model
+    assert result_err == model_err
 
 @pytest.mark.timeout(2)
 def test_echo_param_e_simple():
-    result = os.popen("./42sh -c \"echo -e Hello\"").read()
+    result = subprocess.run(["./42sh","-c", "echo -e Hello"], stdout=subprocess.PIPE).stdout.decode('utf-8')
     model = subprocess.run(["bash", "--posix","-c", "echo -e Hello"], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    
+    result_err = subprocess.run(["./42sh","-c", "echo -e Hello"], stderr=subprocess.PIPE).stderr.decode('utf-8')
+    model_err = subprocess.run(["bash", "--posix","-c", "echo -e Hello"], stderr=subprocess.PIPE).stderr.decode('utf-8')
+    
     assert result == model
+    assert result_err == model_err
 
 @pytest.mark.timeout(2)
 def test_echo_param_e_long():
     result = subprocess.run(["./42sh", "-c", "echo -e Hello Friend"], stdout=subprocess.PIPE).stdout.decode('utf-8')
     model = subprocess.run(["bash", "--posix","-c", "echo -e Hello Friend"], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    
+    result_err = subprocess.run(["./42sh", "-c", "echo -e Hello Friend"], stderr=subprocess.PIPE).stderr.decode('utf-8')
+    model_err = subprocess.run(["bash", "--posix","-c", "echo -e Hello Friend"], stderr=subprocess.PIPE).stderr.decode('utf-8')
+    
     assert result == model
+    assert result_err == model_err
 
 @pytest.mark.timeout(2)
 def test_echo_param_e_long_quotes():
