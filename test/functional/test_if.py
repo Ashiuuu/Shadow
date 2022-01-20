54eaa24098b20d96121f43b11f8268938d853bcc
Nicolas Sutter <nicolas.sutter@epita.fr>
2022-01-20 18:14:47 +0100

    adding wrong syntax if test

--- test/functional/test_if.py
+++ test/functional/test_if.py
@@ -20,3 +20,9 @@ def test_if_cond_test_comma():
     result = subprocess.run(["./42sh", "-c", "if [ 1 -eq 1 ]; then echo b; else echo c; fi"], stdout=subprocess.PIPE).stdout.decode('utf-8')
     model = subprocess.run(["bash", "--posix","-c", "if [ 1 -eq 1 ]; then echo b; else echo c; fi"], stdout=subprocess.PIPE).stdout.decode('utf-8')
     assert result == model
+
+@pytest.mark.timeout(2)
+def test_if_wrong_syntax():
+    result = subprocess.run(["./42sh", "-c", "if echo a; then echo couille;"], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    model = subprocess.run(["bash", "--posix","-c", "if echo a; then echo couille;"], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    assert result == model
