a67a7e27b40f140d94231958710d31d7525ec576
Nicolas Sutter <nicolas.sutter@epita.fr>
2022-01-20 21:56:11 +0100

    leak detect script

--- test/functional/test_script.py
+++ test/functional/test_script.py
@@ -6,16 +6,31 @@ import pytest
 def test_script_echo():
     result = subprocess.run(["./42sh", "test/functional/test.sh"], stdout=subprocess.PIPE).stdout.decode('utf-8')
     model = subprocess.run(["bash", "--posix","test/functional/test.sh"], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    
+    result_err = subprocess.run(["./42sh", "test/functional/test.sh"], stderr=subprocess.PIPE).stderr.decode('utf-8')
+    model_err = subprocess.run(["bash", "--posix","test/functional/test.sh"], stderr=subprocess.PIPE).stderr.decode('utf-8')
+    
     assert result == model
+    assert result_err == model_err
 
 @pytest.mark.timeout(5)
 def test_script_if_success():
     result = subprocess.run(["./42sh", "test/functional/test_if.sh"], stdout=subprocess.PIPE).stdout.decode('utf-8')
     model = subprocess.run(["bash", "--posix","test/functional/test_if.sh"], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    
+    result_err = subprocess.run(["./42sh", "test/functional/test_if.sh"], stderr=subprocess.PIPE).stderr.decode('utf-8')
+    model_err = subprocess.run(["bash", "--posix","test/functional/test_if.sh"], stderr=subprocess.PIPE).stderr.decode('utf-8')
+    
     assert result == model
+    assert result_err == model_err
 
 @pytest.mark.timeout(5)
 def test_script_if_fail():
     result = subprocess.run(["./42sh", "test/functional/test_if_fail.sh"], stdout=subprocess.PIPE).stdout.decode('utf-8')
     model = subprocess.run(["bash", "--posix","test/functional/test_if_fail.sh"], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    
+    result_err = subprocess.run(["./42sh", "test/functional/test_if_fail.sh"], stderr=subprocess.PIPE).stderr.decode('utf-8')
+    model_err = subprocess.run(["bash", "--posix","test/functional/test_if_fail.sh"], stderr=subprocess.PIPE).stderr.decode('utf-8')
+    
     assert result == model
+    assert result_err == model_err
