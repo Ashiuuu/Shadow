28932795a1d0e73845e10a9d5bbfa2a17745eebd
Ashiuuu <ushiuako@gmail.com>
2022-01-21 14:20:56 +0100

    Simplified test framework

--- test/functional/test_if.py
+++ test/functional/test_if.py
@@ -1,50 +1,20 @@
-import subprocess
-import os
 import pytest
+
+import run_test
 
 @pytest.mark.timeout(2)
 def test_if_simple_comma():
-    result = subprocess.run(["./42sh", "-c", "if echo a; then echo b; else echo c; fi"], stdout=subprocess.PIPE).stdout.decode('utf-8')
-    model = subprocess.run(["bash", "--posix","-c", "if echo a; then echo b; else echo c; fi"], stdout=subprocess.PIPE).stdout.decode('utf-8')
-
-    result_err = subprocess.run(["./42sh", "-c", "if echo a; then echo b; else echo c; fi"], stderr=subprocess.PIPE).stderr.decode('utf-8')
-    model_err = subprocess.run(["bash", "--posix","-c", "if echo a; then echo b; else echo c; fi"], stderr=subprocess.PIPE).stderr.decode('utf-8')
-
-    assert result == model
-    assert result_err == model_err
+    run_test.run_test_command("if echo a; then echo b; else echo c; fi")
 
 @pytest.mark.timeout(2)
 def test_if_simple_without_comma():
-    result = subprocess.run(["./42sh", "-c", "if echo a then echo b else echo c fi"], stdout=subprocess.PIPE).stdout.decode('utf-8')
-    model = subprocess.run(["bash", "--posix","-c", "if echo a then echo b else echo c fi"], stdout=subprocess.PIPE).stdout.decode('utf-8')
-
-    result_err = subprocess.run(["./42sh", "-c", "if echo a then echo b else echo c fi"], stderr=subprocess.PIPE).stderr.decode('utf-8')
-    model_err = subprocess.run(["bash", "--posix","-c", "if echo a then echo b else echo c fi"], stderr=subprocess.PIPE).stderr.decode('utf-8')
-
-    assert result == model
-    assert (result_err == '') == (model_err == '')
-    #assert result_err == model_err
+    run_test.run_test_command("if echo a then echo b else echo c fi")
 
 
 @pytest.mark.timeout(2)
 def test_if_cond_test_comma():
-    result = subprocess.run(["./42sh", "-c", "if [ 1 -eq 1 ]; then echo b; else echo c; fi"], stdout=subprocess.PIPE).stdout.decode('utf-8')
-    model = subprocess.run(["bash", "--posix","-c", "if [ 1 -eq 1 ]; then echo b; else echo c; fi"], stdout=subprocess.PIPE).stdout.decode('utf-8')
-    
-    result_err = subprocess.run(["./42sh", "-c", "if [ 1 -eq 1 ]; then echo b; else echo c; fi"], stderr=subprocess.PIPE).stderr.decode('utf-8')
-    model_err = subprocess.run(["bash", "--posix","-c", "if [ 1 -eq 1 ]; then echo b; else echo c; fi"], stderr=subprocess.PIPE).stderr.decode('utf-8')
-    
-    assert result == model
-    assert result_err == model_err
+    run_test.run_test_command("if [ 1 -eq 1 ]; then echo b; else echo c; fi")
 
 @pytest.mark.timeout(2)
 def test_if_wrong_syntax():
-    result = subprocess.run(["./42sh", "-c", "if echo a; then echo couille;"], stdout=subprocess.PIPE).stdout.decode('utf-8')
-    model = subprocess.run(["bash", "--posix", "if echo a; then echo couille;"], stdout=subprocess.PIPE).stdout.decode('utf-8')
-    
-    result_err = subprocess.run(["./42sh", "-c", "if echo a; then echo couille;"], stderr=subprocess.PIPE).stderr.decode('utf-8')
-    model_err = subprocess.run(["bash", "--posix", "if echo a; then echo couille;"], stderr=subprocess.PIPE).stderr.decode('utf-8')
-    
-    assert result == model
-    assert (result_err == '') == (model_err == '')
-    #assert result_err == model_err
+    run_test.run_test_command("if echo a; then echo couille;")
--- test/functional/test_script.py
+++ test/functional/test_script.py
@@ -2,35 +2,16 @@ import subprocess
 import os
 import pytest
 
+import run_test
+
 @pytest.mark.timeout(5)
 def test_script_echo():
-    result = subprocess.run(["./42sh", "test/functional/test.sh"], stdout=subprocess.PIPE).stdout.decode('utf-8')
-    model = subprocess.run(["bash", "--posix","test/functional/test.sh"], stdout=subprocess.PIPE).stdout.decode('utf-8')
-    
-    result_err = subprocess.run(["./42sh", "test/functional/test.sh"], stderr=subprocess.PIPE).stderr.decode('utf-8')
-    model_err = subprocess.run(["bash", "--posix","test/functional/test.sh"], stderr=subprocess.PIPE).stderr.decode('utf-8')
-    
-    assert result == model
-    assert result_err == model_err
+    run_test.run_test_command("test/functional/test.sh")
 
 @pytest.mark.timeout(5)
 def test_script_if_success():
-    result = subprocess.run(["./42sh", "test/functional/test_if.sh"], stdout=subprocess.PIPE).stdout.decode('utf-8')
-    model = subprocess.run(["bash", "--posix","test/functional/test_if.sh"], stdout=subprocess.PIPE).stdout.decode('utf-8')
-    
-    result_err = subprocess.run(["./42sh", "test/functional/test_if.sh"], stderr=subprocess.PIPE).stderr.decode('utf-8')
-    model_err = subprocess.run(["bash", "--posix","test/functional/test_if.sh"], stderr=subprocess.PIPE).stderr.decode('utf-8')
-    
-    assert result == model
-    assert result_err == model_err
+    run_test.run_test_command("test/functional/test_if.sh")
 
 @pytest.mark.timeout(5)
 def test_script_if_fail():
-    result = subprocess.run(["./42sh", "test/functional/test_if_fail.sh"], stdout=subprocess.PIPE).stdout.decode('utf-8')
-    model = subprocess.run(["bash", "--posix","test/functional/test_if_fail.sh"], stdout=subprocess.PIPE).stdout.decode('utf-8')
-    
-    result_err = subprocess.run(["./42sh", "test/functional/test_if_fail.sh"], stderr=subprocess.PIPE).stderr.decode('utf-8')
-    model_err = subprocess.run(["bash", "--posix","test/functional/test_if_fail.sh"], stderr=subprocess.PIPE).stderr.decode('utf-8')
-    
-    assert result == model
-    assert result_err == model_err
+    run_test.run_test_command("test/functional/test_if_fail.sh")
