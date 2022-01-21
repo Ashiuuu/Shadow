b9eaf4830448687c564bc56fe712d33ab4b49d8c
erwan benard <erwan.benard@epita.fr>
2022-01-21 16:23:56 +0100

    add doc file

--- src/lexer/tokens.h
+++ src/lexer/tokens.h
@@ -4,47 +4,113 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * @brief Token enum
+ * 
+ * List of all token type
+ * 
+ */
 enum token_type
 {
-    TOKEN_ERROR,
-    TOKEN_IF, // if keyword
+    /// if keyword
+    TOKEN_IF,
+    /// then keyword
     TOKEN_THEN,
+    /// elif keyword
     TOKEN_ELIF,
+    /// else keyword
     TOKEN_ELSE,
+    /// fi keyword
     TOKEN_FI,
-    TOKEN_SEMICOL, // ; token
-    TOKEN_EOL, // \n token
-    TOKEN_EOF, // EOF token
-    TOKEN_SINGLEQUOTE, // ' token NOT USED
-    TOKEN_DOUBLEQUOTE, // " token NOT USED
-    TOKEN_WORDS, // identifier token, ex : variable name
-    TOKEN_IO_NUMBER, // numerals recognized only in redirection patterns
-    TOKEN_FRED_OUT, // > token (read F(ile) RED(irection) OUT)
-    TOKEN_FRED_IN, // < token
-    TOKEN_FDRED_OUT, // >& token (F(ile)D(escriptor) RED(irection) OUT)
-    TOKEN_FDRED_IN, // <& token
-    TOKEN_FRED_APP, // >> token (F(file) RED(irection) APP(end), redirect to
-                    // file and append to it)
-    TOKEN_BIRED, // <> token, no idea what this is used for but well
-    TOKEN_FRED_FORCE, // >| token, force truncating output file (search google
-                      // for set -o)
-    TOKEN_WHILE, // while keyword
+    /// ; token
+    TOKEN_SEMICOL,
+    /// \n token
+    TOKEN_EOL, 
+    /// EOF token
+    TOKEN_EOF,
+    /// ' token NOT USED
+    TOKEN_SINGLEQUOTE, 
+    /// " token NOT USED
+    TOKEN_DOUBLEQUOTE, 
+    /// identifier token, ex : variable name
+    TOKEN_WORDS, 
+    /// numerals recognized only in redirection patterns
+    TOKEN_IO_NUMBER, 
+    /// > token (read F(ile) RED(irection) OUT)
+    TOKEN_FRED_OUT, 
+    /// < token
+    TOKEN_FRED_IN, 
+    /// >& token (F(ile)D(escriptor) RED(irection) OUT)
+    TOKEN_FDRED_OUT, 
+    /// <& token
+    TOKEN_FDRED_IN, 
+    /// >> token (F(file) RED(irection) APP(end), redirect to file and append to it)
+    TOKEN_FRED_APP, 
+    /// <> token, no idea what this is used for but well
+    TOKEN_BIRED, 
+    /// >| token, force truncating output file (search google for set -o)
+    TOKEN_FRED_FORCE, 
+    /// while keyword
+    TOKEN_WHILE, 
+    /// until keyword
     TOKEN_UNTIL,
+    /// do keyword
     TOKEN_DO,
+    /// done keyword
     TOKEN_DONE,
-    TOKEN_PIPE, // | token
-    TOKEN_PIPE_NEG, // ! token, used for pipe negation
-    TOKEN_AND, // && token
-    TOKEN_OR, // || token
+    /// | token
+    TOKEN_PIPE, 
+    /// ! token, used for pipe negation
+    TOKEN_PIPE_NEG, 
+    /// && token
+    TOKEN_AND, 
+    /// || token
+    TOKEN_OR,
+    /// other keyword or token
+    TOKEN_ERROR,
 };
 
+/**
+ * @brief 
+ * 
+ */
 struct token
 {
+    /// token type
     enum token_type type;
-    char *value; // value if needed, for example in case of TOKEN_WORDS
+    /// value if needed, for example in case of TOKEN_WORDS
+    char *value; 
 };
 
+/**
+ * @brief create a new structure token
+ * 
+ * @param type 
+ * @return struct token* 
+ */
 struct token *token_new(enum token_type type);
-struct token *token_new_word(char *value); // should only be word type
+
+/**
+ * @brief create a new token structure but a token is a word type
+ * 
+ * @param value 
+ * @return struct token* 
+ */
+struct token *token_new_word(char *value);
+
+/**
+ * @brief create a new toen structure with token
+ * 
+ * @param type 
+ * @param value 
+ * @return struct token* 
+ */
 struct token *token_new_with_value(enum token_type type, char *value);
+
+/**
+ * @brief free a token structure
+ * 
+ * @param token 
+ */
 void token_free(struct token *token);
+
--- src/parser/parser.h
+++ src/parser/parser.h
@@ -6,35 +6,200 @@
 #include "exec.h"
 #include "lexer.h"
 
+/**
+ * @brief enum with parser status
+ *
+ * parser status lists.
+ */
 enum parser_status
 {
-    PARSER_ERROR, // we got a syntax error
-    PARSER_FOUND, // parsing went good and we parsed something
-    PARSER_OK, // nothing found, but no error either
+    /// we got a syntax error
+    PARSER_ERROR,
+    /// parsing went good and we parsed something
+    PARSER_FOUND,
+    /// nothing found, but no error either
+    PARSER_OK,
 };
 
+/**
+ * @brief Token redirec
+ *
+ * This function checks if the token is a redirect token.
+ *
+ * @param type
+ * @return int
+ */
 int is_redirec_token(enum token_type type);
 
+/**
+ * @brief A parser that parses a list of tokens
+ *
+ * A list of tokens is parsed, the function returns an enum depending on whether
+ * everything went well.
+ *
+ * @param ast
+ * @param lexer
+ * @return enum parser_status
+ */
 enum parser_status parse_input(struct ast_node **ast, struct lexer *lexer);
+
+/**
+ * @brief A parser that parses a list of simple command token
+ *
+ * A list of tokens is parsed, the function returns an enum depending on whether
+ * everything went well.
+ *
+ * @param ast
+ * @param input
+ * @return enum parser_status
+ */
 enum parser_status parser_simple_command(struct ast_node **ast,
                                          struct lexer *input);
+
+/**
+ * @brief A parser that parses a list of list token
+ *
+ * A list of tokens is parsed, the function returns an enum depending on whether
+ * everything went well.
+ *
+ * @param ast
+ * @param input
+ * @return enum parser_status
+ */
 enum parser_status parse_list(struct ast_node **ast, struct lexer *input);
+
+/**
+ * @brief A parser that parses a list of compund list token
+ *
+ * A list of tokens is parsed, the function returns an enum depending on whether
+ * everything went well.
+ *
+ * @param ast
+ * @param input
+ * @return enum parser_status
+ */
 enum parser_status parse_compound_list(struct ast_node **ast,
                                        struct lexer *input);
 
+/**
+ * @brief A parser that parses a list of else clause token
+ *
+ * A list of tokens is parsed, the function returns an enum depending on whether
+ * everything went well.
+ *
+ * @param ast
+ * @param input
+ * @return enum parser_status
+ */
 enum parser_status parse_else_clause(struct ast_node **ast,
                                      struct lexer *input);
+
+/**
+ * @brief A parser that parses a list of rule if token
+ *
+ * A list of tokens is parsed, the function returns an enum depending on whether
+ * everything went well.
+ *
+ * @param ast
+ * @param input
+ * @return enum parser_status
+ */
 enum parser_status parse_rule_if(struct ast_node **ast, struct lexer *input);
+
+/**
+ * @brief A parser that parses a list of rule while token
+ *
+ * A list of tokens is parsed, the function returns an enum depending on whether
+ * everything went well.
+ *
+ * @param ast
+ * @param input
+ * @return enum parser_status
+ */
 enum parser_status parse_rule_while(struct ast_node **ast, struct lexer *input);
+
+/**
+ * @brief A parser that parses a list of rule until token
+ *
+ * A list of tokens is parsed, the function returns an enum depending on whether
+ * everything went well.
+ *
+ * @param ast
+ * @param input
+ * @return enum parser_status
+ */
 enum parser_status parse_rule_until(struct ast_node **ast, struct lexer *input);
+/**
+ * @brief A parser that parses a list of do group token
+ *
+ * A list of tokens is parsed, the function returns an enum depending on whether
+ * everything went well.
+ *
+ * @param ast
+ * @param input
+ * @return enum parser_status
+ */
 enum parser_status parse_do_group(struct ast_node **ast, struct lexer *input);
 
+/**
+ * @brief A parser that parses a list of pipeline token
+ *
+ * A list of tokens is parsed, the function returns an enum depending on whether
+ * everything went well.
+ *
+ * @param ast
+ * @param input
+ * @return enum parser_status
+ */
 enum parser_status parse_pipeline(struct ast_node **ast, struct lexer *input);
+
+/**
+ * @brief A parser that parses a list of and or token
+ *
+ * A list of tokens is parsed, the function returns an enum depending on whether
+ * everything went well.
+ *
+ * @param ast
+ * @param input
+ * @return enum parser_status
+ */
 enum parser_status parse_and_or(struct ast_node **ast, struct lexer *input);
 
+/**
+ * @brief A parser that parses a list of redirection token
+ *
+ * A list of tokens is parsed, the function returns an enum depending on whether
+ * everything went well.
+ *
+ * @param red
+ * @param input
+ * @return enum parser_status
+ */
 enum parser_status parse_redirection(struct redirection **red,
                                      struct lexer *input);
 
+/**
+ * @brief A parser that parses a list of command token
+ *
+ * A list of tokens is parsed, the function returns an enum depending on whether
+ * everything went well.
+ *
+ * @param ast
+ * @param input
+ * @return enum parser_status
+ */
 enum parser_status parse_command(struct ast_node **ast, struct lexer *input);
+
+/**
+ * @brief A parser that parses a list of shell command token
+ *
+ * A list of tokens is parsed, the function returns an enum depending on whether
+ * everything went well.
+ *
+ * @param ast
+ * @param input
+ * @return enum parser_status
+ */
 enum parser_status parse_shell_command(struct ast_node **ast,
                                        struct lexer *input);
+
