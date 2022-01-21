2cb745348b3a564b9d9de8426103776419d17ca3
erwan benard <erwan.benard@epita.fr>
2022-01-21 21:50:50 +0100

    split test in pytest

--- pytest.sh
+++ pytest.sh
@@ -3,4 +3,13 @@
 python3 -m venv test-env
 source test-env/bin/activate
 pip install pytest
-pytest
+
+if [ $# -eq 1 ];
+then
+    for i in $*;
+    do
+        pytest "$i"
+    done;
+else
+    pytest;
+fi
