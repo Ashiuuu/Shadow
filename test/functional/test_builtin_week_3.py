7c826e8644a134d4bd645d583b20d71d03a40bbe
Nicolas sutter <nicolas.sutter@epita.fr>
2022-01-21 16:53:26 +0100

    week3 test

---
+++ test/functional/test_builtin_week_3.py
@@ -0,0 +1,19 @@
+import pytest
+
+import run_test
+
+@pytest.mark.timeout(2)
+def test_exit():
+    run_test.run_test_command("exit")
+
+@pytest.mark.timeout(2)
+def test_test_var():
+    run_test.run_test_command("PWD=1")
+
+@pytest.mark.timeout(2)
+def test_test_var_display():
+    run_test.run_test_command("echo \"$PWD\"")
+
+@pytest.mark.timeout(2)
+def test_test_function():
+    run_test.run_test_command("foo() { echo this is inside a command block; }")
