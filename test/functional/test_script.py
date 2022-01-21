a6f52630901b71e8e825ad497bf8c7f48ab5fab9
Ashiuuu <ushiuako@gmail.com>
2022-01-21 14:25:46 +0100

    Reverted changes

--- test/functional/test_script.py
+++ test/functional/test_script.py
@@ -1,17 +1,35 @@
 import subprocess
-import os
 import pytest
-
-import run_test
 
 @pytest.mark.timeout(5)
 def test_script_echo():
-    run_test.run_test_command("test/functional/test.sh")
+    result = subprocess.run(["./42sh", "test/functional/test.sh"], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    model = subprocess.run(["bash", "--posix","test/functional/test.sh"], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    
+    result_err = subprocess.run(["./42sh", "test/functional/test.sh"], stderr=subprocess.PIPE).stderr.decode('utf-8')
+    model_err = subprocess.run(["bash", "--posix","test/functional/test.sh"], stderr=subprocess.PIPE).stderr.decode('utf-8')
+    
+    assert result == model
+    assert result_err == model_err
 
 @pytest.mark.timeout(5)
 def test_script_if_success():
-    run_test.run_test_command("test/functional/test_if.sh")
+    result = subprocess.run(["./42sh", "test/functional/test_if.sh"], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    model = subprocess.run(["bash", "--posix","test/functional/test_if.sh"], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    
+    result_err = subprocess.run(["./42sh", "test/functional/test_if.sh"], stderr=subprocess.PIPE).stderr.decode('utf-8')
+    model_err = subprocess.run(["bash", "--posix","test/functional/test_if.sh"], stderr=subprocess.PIPE).stderr.decode('utf-8')
+    
+    assert result == model
+    assert result_err == model_err
 
 @pytest.mark.timeout(5)
 def test_script_if_fail():
-    run_test.run_test_command("test/functional/test_if_fail.sh")
+    result = subprocess.run(["./42sh", "test/functional/test_if_fail.sh"], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    model = subprocess.run(["bash", "--posix","test/functional/test_if_fail.sh"], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    
+    result_err = subprocess.run(["./42sh", "test/functional/test_if_fail.sh"], stderr=subprocess.PIPE).stderr.decode('utf-8')
+    model_err = subprocess.run(["bash", "--posix","test/functional/test_if_fail.sh"], stderr=subprocess.PIPE).stderr.decode('utf-8')
+    
+    assert result == model
+    assert result_err == model_err
