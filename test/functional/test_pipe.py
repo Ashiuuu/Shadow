e4d01863af6039a26562260892cdbb9eb8c3c854
Nicolas sutter <nicolas.sutter@epita.fr>
2022-01-21 15:28:42 +0100

    pipe test

---
+++ test/functional/test_pipe.py
@@ -0,0 +1,27 @@
+import subprocess
+import pytest
+
+import run_test
+
+@pytest.mark.timeout(2)
+def test_pipe_single():
+    run_test.run_test_command("echo a | echo b")
+
+@pytest.mark.timeout(2)
+def test_pipe_double():
+    run_test.run_test_command("echo a | echo b | echo c")
+
+@pytest.mark.timeout(2)
+def test_pipe_triple():
+    run_test.run_test_command("echo a | echo b | echo c | echo d")
+
+@pytest.mark.timeout(2)
+def test_pipe_wrong_syntax():
+    run_test.run_test_command("echo a | echo b |")
+
+@pytest.mark.timeout(2)
+def test_pipe_word_cut():
+    run_test.run_test_command("ls -l | wc")
+
+
+    
