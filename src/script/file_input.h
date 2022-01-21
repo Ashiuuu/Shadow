d9d1fe722905c7567b886750598422c749e64ac9
Ashiuuu <ushiuako@gmail.com>
2022-01-21 15:54:31 +0100

    Merge

--- src/script/file_input.h
+++ src/script/file_input.h
@@ -1,10 +1,10 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #include "exec.h"
 #include "io.h"
