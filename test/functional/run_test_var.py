20c71afb832f2489656f186c4a6c9d19ec9794ea
erwan benard <erwan.benard@epita.fr>
2022-01-21 22:03:35 +0100

    fix error test pytest

--- test/functional/run_test_var.py
+++ test/functional/run_test_var.py
@@ -2,7 +2,7 @@ import subprocess
 import pytest
 
 @pytest.mark.timeout(2)
-def run_test_command(scrypt):
+def run_test_var(scrypt):
     result1 = subprocess.run(["./42sh", "-c", scrypt], stdout=subprocess.PIPE).stdout.decode('utf-8')
     model1 = subprocess.run(["bash", "--posix","-c", scrypt], stdout=subprocess.PIPE).stdout.decode('utf-8')
 
--- test/functional/test_variable_special_with_file.py
+++ test/functional/test_variable_special_with_file.py
@@ -1,14 +1,13 @@
 import run_test_var
-from test.functional.run_test import run_test_command
 
 def test_special_name_scrypt():
-    run_test_var.run_test_command("test/functional/variable/var.sh")
+    run_test_var.run_test_var("test/functional/variable/var.sh")
 
 def test_special_nb_parameter():
-    run_test_var.run_test_command("test/functional/variable/var_param_nb.sh")
+    run_test_var.run_test_var("test/functional/variable/var_param_nb.sh")
 
 def test_special_UID_and_RANDOM():
-    run_test_var.run_test_command("test/functional/variable/var_random.sh")
+    run_test_var.run_test_var("test/functional/variable/var_random.sh")
 
 def test_number_var():
-    run_test_var.run_test_command("test/functional/variable/var_number.sh")
+    run_test_var.run_test_var("test/functional/variable/var_number.sh")
