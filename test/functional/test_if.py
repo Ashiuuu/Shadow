e12b2fea13d2da9391f580456aa379ab39e24457
Nicolas sutter <nicolas.sutter@epita.fr>
2022-01-22 18:59:34 +0100

    test delete

--- test/functional/test_if.py
+++ test/functional/test_if.py
@@ -10,11 +10,6 @@ def test_if_simple_comma():
 def test_if_simple_without_comma():
     run_test.run_test_command("if echo a then echo b else echo c fi")
 
-
-@pytest.mark.timeout(2)
-def test_if_cond_test_comma():
-    run_test.run_test_command("if [ 1 -eq 1 ]; then echo b; else echo c; fi")
-
 @pytest.mark.timeout(2)
 def test_if_wrong_syntax():
     run_test.run_test_command("if echo a; then echo couille;")
