afa651eceef7ba8b057b0bb9931254cece943958
Ashiuuu <ushiuako@gmail.com>
2022-01-23 16:54:06 +0100

    Fixed conflict

--- src/exec/variable_exec.c
+++ src/exec/variable_exec.c
@@ -46,75 +46,19 @@ char *variable_search_and_replace(char *string)
             {
                 if (string[i + 1] == '{')
                 {
-<<<<<<< HEAD
                     name =
                         parse_variable_bracket(string + i + 2); // skip bracket
                     return replace(name, string, i, len);
-=======
-                    name =
-                        parse_variable_bracket(string + i + 2); // skip bracket
-                    size_t name_len = strlen(name) + 2; // { and }
-                    char *value = get_variable(name);
-                    size_t val_len = 0;
-                    if (value != NULL)
-                        val_len = strlen(value);
-                    char *first_half = strndup(string, i);
-                    first_half = xrealloc(
-                        first_half, sizeof(char) * (len + val_len - name_len));
-                    if (value != NULL)
-                        first_half = strcat(first_half, value);
-                    first_half =
-                        strcat(first_half, (string + i + name_len + 1));
-                    free(string);
-                    free(name);
-                    return first_half;
->>>>>>> 3ce8ee2ac054f0ec3303069d52544374cb260013
                 }
                 else if (is_special(string[i + 1]))
                 {
                     name = parse_special(string + i + 1);
-<<<<<<< HEAD
                     return replace(name, string, i, len);
-=======
-                    size_t name_len = strlen(name);
-                    char *value = get_variable(name);
-                    size_t val_len = 0;
-                    if (value != NULL)
-                        val_len = strlen(value);
-                    char *first_half = strndup(string, i);
-                    first_half = xrealloc(
-                        first_half, sizeof(char) * (len + val_len - name_len));
-                    if (value != NULL)
-                        first_half = strcat(first_half, value);
-                    first_half =
-                        strcat(first_half, (string + i + name_len + 1));
-                    free(string);
-                    free(name);
-                    return first_half;
->>>>>>> 3ce8ee2ac054f0ec3303069d52544374cb260013
                 }
                 else if (is_alpha(string[i + 1]))
                 {
                     name = parse_alpha(string + i + 1);
-<<<<<<< HEAD
                     return replace(name, string, i, len);
-=======
-                    size_t name_len = strlen(name);
-                    char *value = get_variable(name);
-                    size_t val_len = 0;
-                    if (value != NULL)
-                        val_len = strlen(value);
-                    char *first_half = strndup(string, i);
-                    first_half = xrealloc(
-                        first_half, sizeof(char) * (len + val_len - name_len));
-                    if (value != NULL)
-                        first_half = strcat(first_half, value);
-                    first_half =
-                        strcat(first_half, (string + i + name_len + 1));
-                    free(string);
-                    free(name);
-                    return first_half;
->>>>>>> 3ce8ee2ac054f0ec3303069d52544374cb260013
                 }
                 else
                     continue;
